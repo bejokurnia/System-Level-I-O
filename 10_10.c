e4d63d490b697601762eee1b38a40b218feafb12
Wakhid_k <kurniawan.wk48@gmail.com>
2020-05-03 17:25:14 +0700

    Update 10_10.c

--- 10_10.c
+++ 10_10.c
@@ -1,11 +1,4 @@
 // **
-// Modify the cpfile program in Figure 10.5 so that it takes an optional command
-// line argument infile. If infile is given, then only copy infile to standard
-// output; otherwise, copy standard input to standard output as before. The
-// twists is that your solution must use the original copy loop (line 9-11) for
-// both cases. You are only allowed to insert code, and you are not allowed to
-// change any of the existing code.
-
 #include "csapp.h"
 
 int main(int argc, char **argv)
@@ -17,7 +10,7 @@ int main(int argc, char **argv)
   if (argc != 1) {
     int fd = open(argv[1], O_RDONLY, 0);
     if (fd == -1) {
-      fprintf(stderr, "open: %s\n", strerror(errno));
+      fprintf(stderr, "Buka: %s\n", strerror(errno));
       return EXIT_FAILURE;
     }
     int stat = dup2(fd, STDIN_FILENO);
